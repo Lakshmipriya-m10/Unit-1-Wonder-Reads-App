a770a6bf341659e123c830b2c1e36b975cd72191
lakshmi priya <lakshmipriyaark@gmail.com>
2026-07-06 13:51:35 -0500

    feature-footer added

--- Wonder-ReadsApp/src/App.jsx
+++ Wonder-ReadsApp/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Footer from './components/Footer';
 function App() {
   return (
       <div className='container'>
         <h1>Hello!LaunchCode</h1>
+        <Footer />
+        
       </div>
      )
 }
---
+++ Wonder-ReadsApp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer">
+                <p>© 2026 Wonder Reads App. All rights reserved.</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
