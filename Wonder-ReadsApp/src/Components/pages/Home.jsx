2fcccc7b34a490b1643988a4808e6ce324402e47
lakshmi priya <lakshmipriyaark@gmail.com>
2026-07-06 17:16:13 -0500

    Complete Home page and Navbar updates

--- Wonder-ReadsApp/src/Components/pages/Home.jsx
+++ Wonder-ReadsApp/src/Components/pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home-page">
+      <h1>Welcome to Wonder Reads</h1>
+      <p>Explore magical stories and books!</p>
     </div>
   )
 }
