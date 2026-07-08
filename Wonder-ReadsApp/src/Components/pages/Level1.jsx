27622fb3c207ce0c901f351f1acb3d7fc71170a2
lakshmi priya <lakshmipriyaark@gmail.com>
2026-07-08 14:43:32 -0500

    images added

--- Wonder-ReadsApp/src/Components/pages/Level1.jsx
+++ Wonder-ReadsApp/src/Components/pages/Level1.jsx
@@ -2,26 +2,51 @@ import React from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
- import 'swiper/css';
+import 'swiper/css';
 
 const Level1 = () => {
-  return (
-    <div>
-      <h1>Level 1</h1>
-      <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Level 1</h1>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <img
+                        src="https://res.cloudinary.com/o7vbtffn/image/upload/v1783474816/r1_v51cl2.jpg"
+                        alt="Story-1"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://res.cloudinary.com/o7vbtffn/image/upload/v1783474816/r1_v51cl2.jpg"
+                        alt="Story-2"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://res.cloudinary.com/o7vbtffn/image/upload/v1783474816/r1_v51cl2.jpg"
+                        alt="Story-3"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://res.cloudinary.com/o7vbtffn/image/upload/v1783474816/r1_v51cl2.jpg"
+                        alt="Story-4"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="https://res.cloudinary.com/o7vbtffn/image/upload/v1783474816/r1_v51cl2.jpg"
+                        alt="Story-5"
+                    />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    )
 }
 
 export default Level1
