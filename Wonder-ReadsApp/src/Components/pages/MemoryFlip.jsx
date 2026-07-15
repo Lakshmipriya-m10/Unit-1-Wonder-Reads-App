111ba62f20d594814d1a5fdf097bcd6f00263125
lakshmi priya <lakshmipriyaark@gmail.com>
2026-07-15 15:16:20 -0500

    memory game logic added

--- Wonder-ReadsApp/src/Components/pages/MemoryFlip.jsx
+++ Wonder-ReadsApp/src/Components/pages/MemoryFlip.jsx
@@ -3,7 +3,6 @@ import Header from '../pages/Header.jsx';
 import '../design/header.css';
 import '../design/memoryflip.css';
 
-
 const shuffleCards = (cards) => {
     return [...cards].sort(() => Math.random() - 0.5);
 };
@@ -12,6 +11,7 @@ const MemoryFlip = () => {
 
     const [firstCard, setFirstCard] = useState(null);
     const [secondCard, setSecondCard] = useState(null);
+    const [lockBoard, setLockBoard] = useState(false);
 
     const backImage =
         "https://res.cloudinary.com/o7vbtffn/image/upload/v1784137975/pm-f_xdfyov.jpg";
@@ -49,18 +49,26 @@ const MemoryFlip = () => {
         }
     ];
 
-
-const [cards, setCards] = useState(
-    shuffleCards(
-        pokemonCards.map(card => ({
-            ...card,
-            flipped: false
-        }))
-    )
-);
-
+    const [cards, setCards] = useState(() =>
+        shuffleCards(
+            pokemonCards.map(card => ({
+                ...card,
+                flipped: false
+            }))
+        )
+    );
 
     const flipCard = (id) => {
+
+        if (lockBoard) return;
+        const clickedCard = cards.find(
+            card => card.id === id
+        );
+
+        // Prevent clicking same card twice
+        if (clickedCard.flipped) return;
+
+        // Flip selected card
         setCards(
             cards.map(card =>
                 card.id === id
@@ -68,32 +76,61 @@ const [cards, setCards] = useState(
                     : card
             )
         );
+
+        if (!firstCard) {
+            setFirstCard(clickedCard);
+        } else {
+            setSecondCard(clickedCard);
+            setLockBoard(true);
+            checkMatch(firstCard, clickedCard);
+        }
     };
 
+    const checkMatch = (card1, card2) => {
+        if (card1.name === card2.name) {
+            // Cards match
+            setFirstCard(null);
+            setSecondCard(null);
+            setLockBoard(false);
+        } else {
+            // Cards do not match
+            setTimeout(() => {
+                setCards(cards =>
+                    cards.map(card =>
+                        card.id === card1.id ||
+                            card.id === card2.id
+                            ? {
+                                ...card,
+                                flipped: false
+                            }
+                            : card
+                    )
+                );
+                setFirstCard(null);
+                setSecondCard(null);
+                setLockBoard(false);
+            }, 1000);
+        }
+    };
 
     return (
         <div>
             <Header title="Pokémon Flip Game" />
             <div className="card-align">
                 <div className="card-grid">
-
                     {cards.map(card => (
                         <div
                             key={card.id}
                             className={`memory-card ${card.flipped ? "flip" : ""}`}
                             onClick={() => flipCard(card.id)}
                         >
-
-
                             <div className="card-inner">
-                                {/* Back Image (hidden initially) */}
                                 <div className="card-back-image">
                                     <img
                                         src={backImage}
                                         alt="card back"
                                     />
                                 </div>
-                                {/* Front Pokemon Image */}
                                 <div className="card-front-image">
                                     <img
                                         src={card.image}
@@ -101,12 +138,11 @@ const [cards, setCards] = useState(
                                     />
                                 </div>
                             </div>
-                        </div>
+                       </div>
                     ))}
                 </div>
             </div>
         </div>
     );
 };
-
 export default MemoryFlip;
